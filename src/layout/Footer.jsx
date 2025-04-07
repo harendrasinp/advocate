e64f95022480b2eaed965a67859bc2661bfac4f0
Harendrasinh <haren111990@gmail.com>
2025-04-07 14:21:28 +0530

    add footer detail

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div className='w-full h-[5rem] bg-slate-700'>
-        <div>Footer</div>
+    <div className='w-full h-[5rem] text-amber-100 bg-slate-700 flex flex-col justify-center items-center'>
+        <div>© 2025 VayuSoftwares</div>
+        <div>Founder Harendrasinh Parmar</div>
+        <div>Site Developer by Vayu Softwares</div>
     </div>
   )
 }

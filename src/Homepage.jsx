bd3a154b59574a262c6a488f7f27167a5ba3e18d
Harendrasinh <haren111990@gmail.com>
2025-04-07 23:05:00 +0530

    add

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { baldevjiDetail } from './data/baldevjiDetail'
 export const Homepage = () => {
   return (
-    <div className='overflow-x-hidden'>
+    <div>
       {/* ----------------------------------------Section One-------------------------------------------------- */}
       <div className='w-[100%] min-h-screen lg:h-[30rem] bg-[url("/images/backgrond.jpg")] bg-cover bg-no-repeat bg-center'>
         <div className='w-[100%] min-h-screen bg-black/65 lg:px-[10rem] lg:py-10'>
@@ -13,7 +13,7 @@ export const Homepage = () => {
               <img className='h-full w-full' src="/images/baldevji2.jpg" alt="jiju" />
             </div>
 
-            <div className='text-amber-50 w-[22rem] flex flex-col justify-center items-center text-justify lg:items-start lg:w-[50rem] px-2'>
+            <div className='text-amber-50 w-[22rem] flex flex-col justify-center items-center text-justify lg:items-start lg:w-[50rem]'>
               <div>{baldevjiDetail.Name}</div>
               <div>{baldevjiDetail.Experience}</div>
               <div>{baldevjiDetail.Specialization}</div>

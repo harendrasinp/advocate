44b01f75284aec52294c1882139701642d984611
Harendrasinh <haren111990@gmail.com>
2025-04-07 21:03:37 +0530

    add overflow hide

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -4,7 +4,7 @@ export const Homepage = () => {
   return (
     <div>
       {/* ----------------------------------------Section One-------------------------------------------------- */}
-      <div className='w-full h-[37rem] lg:h-[30rem]  bg-[url("/images/backgrond.jpg")] bg-cover bg-no-repeat bg-center'>
+      <div className='overflow-hidden w-full h-[37rem] lg:h-[30rem]  bg-[url("/images/backgrond.jpg")] bg-cover bg-no-repeat bg-center'>
         <div className='w-full min-h-screen bg-black/65 lg:px-[10rem] lg:py-10'>
 
           <div className='w-full py-[2rem] bg-slate-500/20 flex justify-center items-center flex-col lg:flex-row lg:items-end lg:py-0'>
@@ -13,7 +13,7 @@ export const Homepage = () => {
               <img className='h-full w-full' src="/images/baldevji2.jpg" alt="jiju" />
             </div>
 
-            <div className='text-amber-50 max-w-[22rem] flex flex-col justify-center items-center text-justify lg:items-start lg:w-[50rem] px-2'>
+            <div className='text-amber-50 w-[22rem] flex flex-col justify-center items-center text-justify lg:items-start lg:w-[50rem] px-2'>
               <div>{baldevjiDetail.Name}</div>
               <div>{baldevjiDetail.Experience}</div>
               <div>{baldevjiDetail.Specialization}</div>

360b7779bf2207420944a02a49b240e70b5a8dfb
Harendrasinh <haren111990@gmail.com>
2025-04-07 20:00:39 +0530

    aline set for associates

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -24,10 +24,16 @@ export const Homepage = () => {
       </div>
       {/* ------------------------------Second Two---------------------------------------------------- */}
       <div className='min-h-screen w-full bg-[url("/images/section2Image.jpg")] bg-cover bg-center bg-no-repeat'>
-        <div className='min-h-screen w-full bg-black/65 flex flex-col justify-center items-center gap-4 lg:flex lg:flex-col lg:justify-center lg:items-center lg:gap-2'>
+        <div className='min-h-screen w-full bg-black/65 flex flex-col justify-center items-center gap-10 lg:flex lg:flex-col lg:justify-center lg:items-center lg:gap-10'>
           <div className='text-3xl text-amber-100'>Associates</div>
 
-          <div className='text-amber-50 flex flex-col justify-center gap-5 lg:flex lg:flex-row lg:h-[20rem] lg:justify-center lg:gap-5'>
+          <div className='text-amber-50 grid grid-cols-2 gap-4 lg:flex lg:flex-row lg:h-[20rem] lg:justify-center lg:gap-5'>
+
+            <div className='w-[10rem] h-[12rem] bg-slate-400/25 p-[0.5rem] lg:w-[13rem] lg:h-[16rem] flex flex-col items-center rounded-[0.2rem]'>
+              <img className='h-[8rem] lg:h-[12rem] w-full' src="/images/baldevji.jpg" alt="associat1" />
+              <div>Harendrasinh</div>
+              <div>LLM (Mumbai Univ)</div>
+            </div>
 
             <div className='w-[10rem] h-[12rem] bg-slate-400/25 p-[0.5rem] lg:w-[13rem] lg:h-[16rem] flex flex-col items-center rounded-[0.2rem]'>
               <img className='h-[8rem] lg:h-[12rem] w-full' src="/images/baldevji.jpg" alt="associat1" />

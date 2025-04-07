b2fe2fca57861813a733f4607f21a07469fe1d81
Harendrasinh <haren111990@gmail.com>
2025-04-07 22:38:26 +0530

    add

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -4,24 +4,24 @@ export const Homepage = () => {
   return (
     <div>
       {/* ----------------------------------------Section One-------------------------------------------------- */}
-      <div className='w-full min-h-full  bg-[url("/images/backgrond.jpg")] bg-cover bg-no-repeat bg-center'>
-      <div className='w-full min-h-screen bg-black/65 lg:px-[10rem] lg:py-10'>
+      <div className='w-[100%] min-h-screen lg:h-[30rem] bg-[url("/images/backgrond.jpg")] bg-cover bg-no-repeat bg-center'>
+        <div className='w-[100%] min-h-screen bg-black/65 lg:px-[10rem] lg:py-10'>
 
-        <div className='w-full py-[2rem] bg-slate-500/20 flex justify-center items-center flex-col lg:flex-row lg:items-end lg:py-0'>
-          
-          <div className='h-[13rem] w-[10rem] lg:h-[25rem] lg:w-[19rem]'>
-            <img className='h-full w-full' src="/images/baldevji2.jpg" alt="jiju" />
-          </div>
+          <div className='w-full py-[2rem] bg-slate-500/20 flex justify-center items-center flex-col lg:flex-row lg:items-end lg:py-0'>
 
-          <div className='text-purple-200 w-[22rem] flex flex-col justify-center items-center text-justify lg:items-start lg:w-[50rem] px-2'>
-            <div>{baldevjiDetail.Name}</div>
-            <div>{baldevjiDetail.Experience}</div>
-            <div>{baldevjiDetail.Specialization}</div>
+            <div className='h-[14rem] w-[12rem] lg:h-[22rem] lg:w-[19rem]'>
+              <img className='h-full w-full' src="/images/baldevji2.jpg" alt="jiju" />
+            </div>
+
+            <div className='text-amber-50 w-[22rem] flex flex-col justify-center items-center text-justify lg:items-start lg:w-[50rem] px-2'>
+              <div>{baldevjiDetail.Name}</div>
+              <div>{baldevjiDetail.Experience}</div>
+              <div>{baldevjiDetail.Specialization}</div>
+            </div>
+
           </div>
         </div>
-
       </div>
-    </div>
       {/* ------------------------------Second Two---------------------------------------------------- */}
       {/* <div className='min-h-screen w-full bg-[url("/images/section2Image.jpg")] bg-cover bg-center bg-no-repeat'>
         <div className='min-h-screen w-full bg-black/65 flex flex-col gap-[1rem]  items-center  lg:flex lg:flex-col lg:justify-center lg:items-center lg:gap-10'>

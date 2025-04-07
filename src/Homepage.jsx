ba1b3423d9a59971a04d9ccd33df0f7f1b4fb435
Harendrasinh <haren111990@gmail.com>
2025-04-07 10:32:36 +0530

    add image to hom page background

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export const Homepage = () => {
   return (
-    <div>home</div>
+    <div className='w-full h-screen bg-[url("/images/backgrond.jpg")] bg-cover bg-no-repeat bg-center'>
+        <div className=' w-full h-screen bg-black/75'></div>
+    </div>
   )
 }

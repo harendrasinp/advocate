d6f9826c7759705104a1c4e514dbfee1fe716c5b
Harendrasinh <haren111990@gmail.com>
2025-04-07 20:40:36 +0530

    update new

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -27,7 +27,7 @@ export const Homepage = () => {
         <div className='min-h-screen w-full bg-black/65 flex flex-col gap-[2rem]  items-center  lg:flex lg:flex-col lg:justify-center lg:items-center lg:gap-10'>
           <div className='text-3xl text-amber-100'>Associates</div>
 
-          <div className='text-amber-50 grid bg-amber-500 grid-cols-2 gap-4 lg:flex lg:flex-row lg:h-[20rem] lg:justify-center lg:gap-5'>
+          <div className='text-amber-50 grid grid-cols-2 gap-4 lg:flex lg:flex-row lg:h-[20rem] lg:justify-center lg:gap-5'>
 
             <div className='w-[10rem] h-[13rem] bg-slate-400/25 p-[0.5rem] lg:w-[13rem] lg:h-[16rem] flex flex-col items-center rounded-[0.2rem]'>
               <img className='h-[9rem] lg:h-[12rem] w-full' src="/images/baldevji.jpg" alt="associat1" />

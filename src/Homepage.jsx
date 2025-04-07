7857b8c87195570b8bbf298dbed8610e678ebaef
Harendrasinh <haren111990@gmail.com>
2025-04-07 19:53:01 +0530

    set position of associates

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -9,7 +9,7 @@ export const Homepage = () => {
 
           <div className='w-full py-[2rem] bg-slate-500/20 flex justify-center items-center flex-col lg:flex-row lg:items-end lg:py-0'>
 
-            <div className='h-[13rem] w-[10rem] lg:h-[25rem] lg:w-[19rem]'>
+            <div className='h-[14rem] w-[12rem] lg:h-[25rem] lg:w-[19rem]'>
               <img className='h-full w-full' src="/images/baldevji2.jpg" alt="jiju" />
             </div>
 
@@ -24,25 +24,27 @@ export const Homepage = () => {
       </div>
       {/* ------------------------------Second Two---------------------------------------------------- */}
       <div className='min-h-screen w-full bg-[url("/images/section2Image.jpg")] bg-cover bg-center bg-no-repeat'>
-        <div className='min-h-screen w-full bg-black/65'>
-          <div className='text-amber-50 flex flex-col justify-center items-center gap-5 lg:flex lg:flex-row lg:h-[20rem] lg:justify-center lg:gap-5 '>
+        <div className='min-h-screen w-full bg-black/65 flex flex-col justify-center items-center gap-4 lg:flex lg:flex-col lg:justify-center lg:items-center lg:gap-2'>
+          <div className='text-3xl text-amber-100'>Associates</div>
 
-            <div className='bg-slate-400/25 p-[0.5rem] w-[13rem] h-[16rem] flex flex-col items-center rounded-[0.2rem]'>
-                <img className=' relative h-[12rem] w-full' src="/images/baldevji.jpg" alt="associat1" />
-                <div>Harendrasinh</div>
-                <div>LLM (Mumbai Univercity)</div>
+          <div className='text-amber-50 flex flex-col justify-center gap-5 lg:flex lg:flex-row lg:h-[20rem] lg:justify-center lg:gap-5'>
+
+            <div className='w-[10rem] h-[12rem] bg-slate-400/25 p-[0.5rem] lg:w-[13rem] lg:h-[16rem] flex flex-col items-center rounded-[0.2rem]'>
+              <img className='h-[8rem] lg:h-[12rem] w-full' src="/images/baldevji.jpg" alt="associat1" />
+              <div>Harendrasinh</div>
+              <div>LLM (Mumbai Univ)</div>
             </div>
 
-            <div className='bg-slate-400/25 p-[0.5rem] w-[13rem] h-[16rem] flex flex-col items-center rounded-[0.2rem]'>
-                <img className=' relative h-[12rem] w-full' src="/images/baldevji.jpg" alt="associat1" />
-                <div>Harendrasinh</div>
-                <div>LLM (Mumbai Univercity)</div>
+            <div className='w-[10rem] h-[12rem] bg-slate-400/25 p-[0.5rem] lg:w-[13rem] lg:h-[16rem] flex flex-col items-center rounded-[0.2rem]'>
+              <img className='h-[8rem] lg:h-[12rem] w-full' src="/images/baldevji.jpg" alt="associat1" />
+              <div>Harendrasinh</div>
+              <div>LLM (Mumbai Univ)</div>
             </div>
 
-            <div className='bg-slate-400/25 p-[0.5rem] w-[13rem] h-[16rem] flex flex-col items-center rounded-[0.2rem]'>
-                <img className=' relative h-[12rem] w-full' src="/images/baldevji.jpg" alt="associat1" />
-                <div>Harendrasinh</div>
-                <div>LLM (Mumbai Univercity)</div>
+            <div className='w-[10rem] h-[12rem] bg-slate-400/25 p-[0.5rem] lg:w-[13rem] lg:h-[16rem] flex flex-col items-center rounded-[0.2rem]'>
+              <img className='h-[8rem] lg:h-[12rem] w-full' src="/images/baldevji.jpg" alt="associat1" />
+              <div>Harendrasinh</div>
+              <div>LLM (Mumbai Univ)</div>
             </div>
 
           </div>

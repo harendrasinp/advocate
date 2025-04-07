c850947b2379edb795dafaaa66359d395fc9bb93
Harendrasinh <haren111990@gmail.com>
2025-04-07 21:08:58 +0530

    white space error

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -4,7 +4,7 @@ export const Homepage = () => {
   return (
     <div>
       {/* ----------------------------------------Section One-------------------------------------------------- */}
-      <div className='overflow-hidden w-full h-[37rem] lg:h-[30rem]  bg-[url("/images/backgrond.jpg")] bg-cover bg-no-repeat bg-center'>
+      <div className='w-full h-[37rem] lg:h-[30rem]  bg-[url("/images/backgrond.jpg")] bg-cover bg-no-repeat bg-center'>
         <div className='w-full min-h-screen bg-black/65 lg:px-[10rem] lg:py-10'>
 
           <div className='w-full py-[2rem] bg-slate-500/20 flex justify-center items-center flex-col lg:flex-row lg:items-end lg:py-0'>
@@ -27,7 +27,7 @@ export const Homepage = () => {
         <div className='min-h-screen w-full bg-black/65 flex flex-col gap-[2rem]  items-center  lg:flex lg:flex-col lg:justify-center lg:items-center lg:gap-10'>
           <div className='text-3xl text-amber-100'>Associates</div>
 
-          <div className='text-amber-50 grid grid-cols-2 gap-4 lg:flex lg:flex-row lg:h-[20rem] lg:justify-center lg:gap-5'>
+          <div className='text-amber-50 lg:flex lg:flex-row lg:h-[20rem] lg:justify-center lg:gap-5'>
 
             <div className='w-[10rem] h-[13rem] bg-slate-400/25 p-[0.5rem] lg:w-[13rem] lg:h-[16rem] flex flex-col items-center rounded-[0.2rem]'>
               <img className='h-[9rem] lg:h-[12rem] w-full' src="/images/baldevji.jpg" alt="associat1" />

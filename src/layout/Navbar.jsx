7ce82ef2b75bd794f10cb7cfd2a824e72e1cda99
Harendrasinh <haren111990@gmail.com>
2025-04-08 22:45:18 +0530

    addd

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -6,7 +6,7 @@ import { Menuitem } from '../Navbar/Menuitem'
 export const Navbar = () => {
   return (
     <div className='max-w-full h-[8rem] bg-gray-900 flex flex-col justify-end items-center lg:h-[9rem]'>
-      <div className='w-[95%] mb-2 flex justify-between items-center lg:mb-1'>
+      <div className='mb-2 flex justify-between items-center lg:mb-1'>
         <Logosection />
         <Humburg />
         <Socialmedia />

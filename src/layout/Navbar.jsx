3456f0be478166d4a52a472d533dd8089033d258
Harendrasinh <haren111990@gmail.com>
2025-04-08 22:48:35 +0530

    add

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -5,8 +5,8 @@ import { Socialmedia } from '../Navbar/Socialmedia'
 import { Menuitem } from '../Navbar/Menuitem'
 export const Navbar = () => {
   return (
-    <div className='max-w-full h-[8rem] bg-gray-900 flex flex-col justify-end items-center lg:h-[9rem]'>
-      <div className='mb-2 flex justify-between items-center lg:mb-1'>
+    <div className='relative max-w-full h-[8rem] bg-gray-900 flex flex-col justify-end items-center lg:h-[9rem]'>
+      <div className='w-[95%] mb-2 flex justify-between items-center lg:mb-1'>
         <Logosection />
         <Humburg />
         <Socialmedia />

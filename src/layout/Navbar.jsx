d3d2dc95e59e4a3b26d971bb3b07397d2574a82e
Harendrasinh <haren111990@gmail.com>
2025-04-08 22:59:35 +0530

    white space remove

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -5,7 +5,7 @@ import { Socialmedia } from '../Navbar/Socialmedia'
 import { Menuitem } from '../Navbar/Menuitem'
 export const Navbar = () => {
   return (
-    <div className='relative max-w-full h-[8rem] bg-gray-900 flex flex-col justify-end items-center lg:h-[9rem]'>
+    <div className='max-w-full h-[8rem] bg-gray-900 flex flex-col justify-end items-center lg:h-[9rem]'>
       <div className='w-[95%] mb-2 flex justify-between items-center lg:mb-1'>
         <Logosection />
         <Humburg />

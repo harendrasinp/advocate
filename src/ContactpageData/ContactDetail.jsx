a30024fdfe70db84d02b202f125e8d6d654a0f11
Harendrasinh <haren111990@gmail.com>
2025-04-11 10:38:26 +0530

    contact page set

--- src/ContactpageData/ContactDetail.jsx
+++ src/ContactpageData/ContactDetail.jsx
@@ -9,8 +9,7 @@ export const ContactDetail = () => {
 
   }, [])
   return (
-    <div ref={animateRef} className='w-full h-[100rem] mt-0 bg-slate-700/50 lg:w-[60rem] lg:h-[25rem] lg:mt-5 rounded-md'>
-
+    <div ref={animateRef} className='w-full h-[35rem] mt-5 bg-slate-700/50 lg:w-[60rem] lg:h-[25rem] lg:mt-5 rounded-md'>
       <div className='barlow-condensed-thin text-amber-400 text-[1.4rem] border-b-2 border-amber-800 shadow-amber-800
                     lg:shadow-2xl flex justify-center items-center lg:text-2xl'>Contact Detail</div>
  
--- src/Main_Pages/Contact.jsx
+++ src/Main_Pages/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { ContactDetail } from '../ContactpageData/ContactDetail'
 export const Contact = () => {
   return (
-    <div className='min-h-screen px-1 w-full bg-[url("/images/contactImage.jpg")] bg-center bg-cover bg-no-repeat 
+    <div className='min-h-screen px-1 w-full bg-[url("/images/contactImage.jpg")] flex justify-center bg-center bg-cover bg-no-repeat 
           lg:flex lg:justify-center lg:px-20'>
       <ContactDetail/>
     </div>

d2e7dd0539eba4f6ec5fb27ff9c0a062b544df20
Harendrasinh <haren111990@gmail.com>
2025-04-11 11:17:54 +0530

    add icon to contact detail

--- src/ContactpageData/ContactDetail.jsx
+++ src/ContactpageData/ContactDetail.jsx
@@ -13,12 +13,42 @@ export const ContactDetail = () => {
     <div ref={animateRef} className='w-full h-[35rem] mt-5 bg-slate-700/50 lg:w-[60rem] lg:h-[25rem] lg:mt-5 rounded-md'>
       <div className='barlow-condensed-thin text-amber-400 text-[1.4rem] border-b-2 border-amber-800 shadow-amber-800
                     lg:shadow-2xl flex justify-center items-center lg:text-2xl'>Contact Detail</div>
-          <div className='text-white flex flex-col justify-center items-center h-[20rem] gap-4'>
-              Email<div className='text-amber-200'>{contactetail.Email}</div>
-              Phone<div className='text-amber-200'>{contactetail.Phone}</div>
-              Office Tel<div className='text-amber-200'>{contactetail.Office_Tel}</div>
-              Address <div className='text-amber-200'>{contactetail.Office_Address}</div>
+
+      <div className='text-white flex flex-col justify-center items-center h-[20rem] gap-4'>
+
+        <div className='flex flex-col justify-center items-center'>
+          <div className='flex justify-center items-center gap-2'>
+            <div>Email</div>
+            <img className='w-[1.5rem] h-[1.5rem]' src="https://cdn-icons-png.flaticon.com/128/2965/2965306.png" alt="email" />
           </div>
+          <div className='text-amber-200'>{contactetail.Email}</div>
+        </div>
+
+        <div className='flex flex-col justify-center items-center'>
+          <div className='flex justify-center items-center gap-2'>
+            <div>Phone</div>
+            <img className='w-[1.5rem] h-[1.5rem]' src="https://cdn-icons-png.flaticon.com/128/3014/3014736.png" alt="email" />
+          </div>
+          <div className='text-amber-200'>{contactetail.Phone}</div>
+        </div>
+
+        <div className='flex flex-col justify-center items-center'>
+          <div className='flex justify-center items-center gap-2'>
+            <div>Office Tel</div>
+            <img className='w-[1.5rem] h-[1.5rem]' src="https://cdn-icons-png.flaticon.com/128/2208/2208224.png" alt="email" />
+          </div>
+          <div className='text-amber-200'>{contactetail.Office_Tel}</div>
+        </div>
+
+        <div className='flex flex-col justify-center items-center'>
+          <div className='flex justify-center items-center gap-2'>
+            <div>Address</div>
+            <img className='w-[1.5rem] h-[1.5rem]' src="https://cdn-icons-png.flaticon.com/128/854/854901.png" alt="email" />
+          </div>
+          <div className='text-amber-200'>{contactetail.Office_Address}</div>
+        </div>
+
+      </div>
     </div>
   )
 }
